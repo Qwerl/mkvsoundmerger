e9d09f5c231824e9ce713a0abc2d439d2627b332
Bikmetov <Askar.Bikmetov@gmail.com>
2020-07-30 22:14:00 +0300

    add new way to check link between files by numbers in file names

--- src/test/java/ru/qwerl/mkvsoundmerger/AppTest.java
+++ src/test/java/ru/qwerl/mkvsoundmerger/AppTest.java
@@ -2,7 +2,6 @@ package ru.qwerl.mkvsoundmerger;
 
 import java.io.File;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -13,8 +12,8 @@ import static org.junit.Assert.assertTrue;
 public class AppTest {
 
   private final static App APP = new App();
-  //-video "<project path>/src/test/resource/test_hierarchy/Elfen Lied [BDRemux 1080p]" -sound "Rus Sounds" "Eng Sounds" -console
-  private final static String[] ARG_EXAMPLE = new String[] {
+
+  private final static String[] VIDEO_AND_SOUND_EQUALS_NAMES = new String[] {
       "-video", new File("src/test/data/test_hierarchy/Elfen Lied [BDRemux 1080p]").getAbsolutePath(),
       "-sound", "Rus Sounds/first", "Eng Sounds",
       "-console",
@@ -23,9 +22,25 @@ public class AppTest {
   };
 
   @Test
+  //-video "<project path>/src/test/resource/test_hierarchy/Elfen Lied [BDRemux 1080p]" -sound "Rus Sounds" "Eng Sounds" -console
   public void testApp() {
-    APP.run(ARG_EXAMPLE);
+    APP.run(VIDEO_AND_SOUND_EQUALS_NAMES);
     assertTrue(true);
   }
+
+  private final static String[] VIDEO_AND_SOUND_DIFFERENT_NAMES = new String[] {
+      "-video", new File("src/test/data/test_hierarchy/Boku dake ga Inai Machi [TV][BDRemux][2016]").getAbsolutePath(),
+      "-search",
+      "-console"
+  };
+
+  @Test
+  //-video 'Boku dake ga Inai Machi [TV][BDRemux][2016]' -search -console
+  public void testApp2() {
+    APP.run(VIDEO_AND_SOUND_DIFFERENT_NAMES);
+    assertTrue(true);
+  }
+
+  //TODO: create CommandHandler to provide commands for tests
 
 }

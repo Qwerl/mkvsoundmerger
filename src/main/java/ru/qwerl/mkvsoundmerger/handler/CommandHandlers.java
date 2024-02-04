791743c0c03445a1c501dce0af50b9303814a37b
askar <askar@intentomatic.com>
2024-02-04 22:11:59 +0600

    add progress indicator to CommandHandlers

--- src/main/java/ru/qwerl/mkvsoundmerger/handler/CommandHandlers.java
+++ src/main/java/ru/qwerl/mkvsoundmerger/handler/CommandHandlers.java
@@ -1,16 +1,19 @@
 package ru.qwerl.mkvsoundmerger.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.qwerl.mkvsoundmerger.builder.Command;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class CommandHandlers extends ArrayList<CommandHandler> {
 
   public void pushCommands(List<Command> commands) {
-    commands.forEach(command ->
-        this.forEach(commandHandler -> commandHandler
-            .handleCommands(command)));
+    for (int i = 0, commandsSize = commands.size(); i < commandsSize; i++) {
+      Command command = commands.get(i);
+      log.info("Handling command[{}/{}]", i+1, commandsSize);
+      forEach(commandHandler -> commandHandler.handleCommands(command));
+    }
   }
-
 }
